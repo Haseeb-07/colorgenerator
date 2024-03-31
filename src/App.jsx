29784b931b9b1b53013db928f97d4f28ae6b5af2
Haseeb-07 <haseebalichaudhary3@gmail.com>
2024-03-31 23:01:27 +0500

    color generator

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Color from './Color'
+import { Toaster } from 'react-hot-toast';
+export default function App() {
+  return (
+    <div>
+    <Toaster />
+      <Color />
+    </div>
+  )
+}
